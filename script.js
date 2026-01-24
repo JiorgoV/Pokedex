ea79dfe8c5c6eea4853606715470ad84b0486caa
Georgios Vavitsas <g.vavitsas@gmail.com>
2026-01-24 16:04:39 +0100

    show pokemon and types

--- script.js
+++ script.js
@@ -5,9 +5,6 @@ const LIMIT = 20;
 async function onloadFunc() {
     let pokemonResponse = await loadData("pokemon?limit=40&offset=0")
     let pokemonList = pokemonResponse.results;  // das ist mein array
-    console.log(pokemonResponse);
-    console.log(pokemonResponse.results);
-
     pokemonList.forEach((pokemon) => {   // hier durch das array gehen
         console.log(pokemon.name);  
     });
@@ -49,16 +46,45 @@ function renderPokemon(pokemonList) {
     let mainContent = document.getElementById('main-content');
     
     pokemonList.forEach((pokemon) => {
-        // Das Bild ist hier versteckt:
         let imageUrl = pokemon.sprites.front_default;
+
+        let typesHTML = '';
+        pokemon.types.forEach((type) => {
+            let emoji = getTypeEmoji(type.type.name);
+            typesHTML += `<button class="type-badge ${type.type.name}">${emoji}</button>`;
+        });
         
         mainContent.innerHTML += `
             <div class="pokemon-card">
-                <img src="${imageUrl}" alt="${pokemon.name}">
-                <h3>${pokemon.name}</h3>
+            <div class="id-name"> <h3>#${pokemon.id} <h3>${pokemon.name}</h3></div>
+            <div class="pokemon-img"><img class="pkm-img" src="${imageUrl}" alt="${pokemon.name}"></div>
+            <div class="pokemon-types">${typesHTML}</div>
             </div>
+        </div>
         `;
     });
 }
 
-
+function getTypeEmoji(typeName) {
+    const typeEmojis = {
+        fire: '🔥',
+        water: '💧',
+        grass: '🌿',
+        electric: '⚡',
+        ice: '❄️',
+        fighting: '👊',
+        poison: '☠️',
+        ground: '🌍',
+        flying: '🪽',
+        psychic: '🔮',
+        bug: '🐛',
+        rock: '🪨',
+        ghost: '👻',
+        dragon: '🐉',
+        dark: '🌙',
+        steel: '⚙️',
+        fairy: '🧚',
+        normal: '⭐'
+    };
+    return typeEmojis[typeName] || '❓';
+}
